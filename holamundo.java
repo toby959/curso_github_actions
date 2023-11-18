b89234a5dfa27c5a530629f41197825a4ab0e43f
toby959 <134654437+toby959@users.noreply.github.com>
2023-11-18 19:39:00 -0300

    Delete holamundo.java

--- holamundo.java
+++
@@ -1,6 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        String nombre = System.getenv("USERNAME");
-        System.out.println("¡Hola, " + nombre + " desde GitHub!");
-    }
-}
