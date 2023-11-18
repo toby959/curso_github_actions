95aeb2670fb7a82c2bc1f40bedacb133424f0038
toby959 <134654437+toby959@users.noreply.github.com>
2023-11-18 19:59:39 -0300

    Create holamundo.java

---
+++ holamundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = System.getenv("USERNAME");
+        System.out.println("¡Hola, " + nombre + " desde GitHub!");
+    }
+}
